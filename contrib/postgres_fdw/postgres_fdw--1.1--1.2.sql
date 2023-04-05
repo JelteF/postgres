8cd9f12bca5a8e2f90eecd012eb800b260c44ef6
Hayato Kuroda <kuroda.hayato@fujitsu.com>
2023-04-05 06:10:51 +0000

    Extend postgres_fdw_get_connections to return user name

--- contrib/postgres_fdw/postgres_fdw--1.1--1.2.sql
+++ contrib/postgres_fdw/postgres_fdw--1.1--1.2.sql
@@ -3,6 +3,14 @@
 -- complain if script is sourced in psql, rather than via ALTER EXTENSION
 \echo Use "ALTER EXTENSION postgres_fdw UPDATE TO '1.2'" to load this file. \quit
 
+DROP FUNCTION postgres_fdw_get_connections ();
+
+CREATE FUNCTION postgres_fdw_get_connections (OUT server_name text,
+    OUT user_name text, OUT valid boolean)
+RETURNS SETOF record
+AS 'MODULE_PATHNAME'
+LANGUAGE C STRICT PARALLEL RESTRICTED;
+
 CREATE FUNCTION postgres_fdw_verify_connection (IN server_name text,
         IN user_name text)
 RETURNS bool
