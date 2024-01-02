e5cf83ec82b6bd3f2f33721833dd8fb4a353e3ac
Jelte Fennema-Nio <jelte.fennema@microsoft.com>
2024-01-02 11:16:19 +0100

    libpq: Handle NegotiateProtocolVersion message more leniently
    Previously libpq would always error when the server returns a
    NegotiateProtocolVersion message. This was fine because libpq only
    supported a single protocol version and did not support any protocol
    parameters. But we now that we're discussing protocol changes we need to
    change this behaviour, and introduce a fallback mechanism when
    connecting to an older server.
    
    This patch modifies the client side checks to allow a range of supported
    protocol versions, instead of only allowing the exact version that was
    requested. In addition it now allows connecting when the server does not
    support some of the requested protocol parameters.
    
    Note that at the moment this change does not have any behavioural
    effect, because libpq will only request version 3.0 and will never send
    protocol parameters. Which means that the client never receives a
    NegotiateProtocolVersion message from the server.

--- src/interfaces/libpq/fe-connect.c
+++ src/interfaces/libpq/fe-connect.c
@@ -1884,11 +1884,30 @@ pqConnectOptions2(PGconn *conn)
 	else
 	{
 		/*
-		 * To not break connecting to older servers/poolers that do not yet
-		 * support NegotiateProtocolVersion, default to the 3.0 protocol at
-		 * least for a while longer.
+		 * Since some old servers and poolers don't support the
+		 * NegotiateProtocolVersion message. So we don't want to send a
+		 * StartupMessage that contains protocol parameters or a non-3.0
+		 * protocol version by default, since either of those would cause a
+		 * failure to connect to an old server. But as soon as the provided
+		 * connection string requires a protocol parameter, we might as well
+		 * ask for the latest protocol version we support by default too.
 		 */
-		conn->max_pversion = PG_PROTOCOL(3, 0);
+		bool		needs_new_protocol_features = false;
+
+		for (const pg_protocol_parameter *param = KnownProtocolParameters; param->name; param++)
+		{
+			const char *value = *(char **) ((char *) conn + param->conn_connection_string_value_offset);
+
+			if (value && value[0])
+			{
+				needs_new_protocol_features = true;
+				break;
+			}
+		}
+		if (needs_new_protocol_features)
+			conn->max_pversion = PG_PROTOCOL_LATEST;
+		else
+			conn->max_pversion = PG_PROTOCOL(3, 0);
 	}
 
 	/*
@@ -3903,7 +3922,7 @@ keep_going:						/* We will come back to here until there is
 					}
 					/* OK, we read the message; mark data consumed */
 					conn->inStart = conn->inCursor;
-					goto error_return;
+					goto keep_going;
 				}
 
 				/* It is an authentication request. */
@@ -4696,6 +4715,17 @@ freePGconn(PGconn *conn)
 		(void) conn->events[i].proc(PGEVT_CONNDESTROY, &evt,
 									conn->events[i].passThrough);
 		free(conn->events[i].name);
+	}
+
+	for (const pg_protocol_parameter *param = KnownProtocolParameters; param->name; param++)
+	{
+		char	   *conn_string_value = *(char **) ((char *) conn + param->conn_connection_string_value_offset);
+		char	   *server_value = *(char **) ((char *) conn + param->conn_server_value_offset);
+		char	   *supported_value = *(char **) ((char *) conn + param->conn_server_support_offset);
+
+		free(conn_string_value);
+		free(server_value);
+		free(supported_value);
 	}
 
 	release_conn_addrinfo(conn);
