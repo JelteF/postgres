3db99dd001b64ffaf1a686bcfed4c7b8eecf74b9
Jelte Fennema-Nio <postgres@jeltef.nl>
2024-08-14 18:25:16 +0200

    Add infrastructure for protocol parameters
    Since the introduction of the NegotiateProtocolParameter message the
    server has been able to say to the client that it doesn't support any of
    the protocol parameters that the client requested.
    
    While this is important for backwards compatibility, it doesn't give
    much guidance for people wanting to add new protocol parameters. This
    commit intends to change that by adding a generic infrastructure that
    can be used to introduce new protocol parameters in a standardized way.
    
    There are two key features that are necessary to actually be able to use
    protocol parameters:
    
    1. Negotiating the valid values of a protocol parameter (e.g. what
       compression methods are supported). This is needed because we want to
       support protocol parameters without adding an extra round-trip to the
       connection startup. So, a server needs to be able to accept the data
       in the StartupMessage, while also sharing with the client what it
       actually accepts in its response.
    2. Changing a protocol parameter after connection startup. This is
       critical for connection poolers, otherwise they would need to
       separate connections with different values for the protocol
       parameters.
    
    To support these two features this commit adds three new protocol
    messages, including their code to handle these messages client and
    server side:
    
    1. NegotiateProtocolParameter (BE): Sent during connection startup when the
       server supports the protocol parameter. This tells the client if the
       server accepted the value that the client provided for the parameter.
       It also tells the client what other values it accepts.
    2. SetProtocolParameter (FE): Can be used to change protocol parameters after
       the connection startup.
    3. SetProtocolParameterComplete (BE): Response to SetProtocolParameter
       which tells the client if the new value was accepted or not.

--- src/interfaces/libpq/fe-connect.c
+++ src/interfaces/libpq/fe-connect.c
@@ -2109,12 +2109,33 @@ pqConnectOptions2(PGconn *conn)
 	else
 	{
 		/*
-		 * To not break connecting to older servers/poolers that do not yet
-		 * support NegotiateProtocolVersion, default to the 3.0 protocol at
-		 * least for a while longer. Except when min_protocol_version is set
-		 * to something larger, then we might as well default to the latest.
+		 * Some old servers and poolers don't support the
+		 * NegotiateProtocolVersion message, so we don't want to send a
+		 * StartupMessage that contains protocol parameters or a non-3.0
+		 * protocol version by default, since either of those would cause a
+		 * failure to connect to an old server. But as soon as the provided
+		 * connection string requires a protocol parameter, we might as well
+		 * default to the latest protocol version too. The same applies if the
+		 * min_protocol_version is set to anything greater than 3.0.
 		 */
-		if (conn->min_pversion > PG_PROTOCOL(3, 0))
+		bool		needs_new_protocol_features = conn->min_pversion > PG_PROTOCOL(3, 0);
+
+		if (!needs_new_protocol_features)
+		{
+			/* Check if any protocol parameter is set */
+			for (const pg_protocol_parameter *param = KnownProtocolParameters; param->name; param++)
+			{
+				const char *value = *(char **) ((char *) conn + param->conn_connection_string_value_offset);
+
+				if (value != NULL && value[0] != '\0')
+				{
+					needs_new_protocol_features = true;
+					break;
+				}
+			}
+		}
+
+		if (needs_new_protocol_features)
 			conn->max_pversion = PG_PROTOCOL_LATEST;
 		else
 			conn->max_pversion = PG_PROTOCOL(3, 0);
@@ -3997,7 +4018,8 @@ keep_going:						/* We will come back to here until there is
 				 */
 				if (beresp != PqMsg_AuthenticationRequest &&
 					beresp != PqMsg_ErrorResponse &&
-					beresp != PqMsg_NegotiateProtocolVersion)
+					beresp != PqMsg_NegotiateProtocolVersion &&
+					beresp != PqMsg_NegotiateProtocolParameter)
 				{
 					libpq_append_conn_error(conn, "expected authentication request from server, but received %c",
 											beresp);
@@ -4152,6 +4174,20 @@ keep_going:						/* We will come back to here until there is
 						goto error_return;
 					}
 
+					goto keep_going;
+				}
+				else if (beresp == PqMsg_NegotiateProtocolParameter)
+				{
+					if (pqGetNegotiateProtocolParameter(conn))
+					{
+						libpq_append_conn_error(conn, "received invalid NegotiateProtocolParameter message");
+						goto error_return;
+					}
+
+					if (conn->error_result)
+						goto error_return;
+
+					pqParseDone(conn, conn->inCursor);
 					goto keep_going;
 				}
 
@@ -5038,6 +5074,17 @@ freePGconn(PGconn *conn)
 		(void) conn->events[i].proc(PGEVT_CONNDESTROY, &evt,
 									conn->events[i].passThrough);
 		free(conn->events[i].name);
+	}
+
+	for (const pg_protocol_parameter *param = KnownProtocolParameters; param->name; param++)
+	{
+		char	   *conn_string_value = *(char **) ((char *) conn + param->conn_connection_string_value_offset);
+		char	   *server_value = *(char **) ((char *) conn + param->conn_server_value_offset);
+		char	   *supported_value = *(char **) ((char *) conn + param->conn_server_support_offset);
+
+		free(conn_string_value);
+		free(server_value);
+		free(supported_value);
 	}
 
 	release_conn_addrinfo(conn);
