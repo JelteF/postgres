9e3414aa41caf6e601c892a7a9d2b5b3526d9839
Jelte Fennema-Nio <postgres@jeltef.nl>
2025-12-03 23:33:18 +0100

    Inline functions that have now become trivial

--- src/backend/utils/cache/funccache.c
+++ src/backend/utils/cache/funccache.c
@@ -51,24 +51,6 @@ static int	cfunc_match(const void *key1, const void *key2, Size keysize);
 
 
 /*
- * Initialize the hash table on first use.
- *
- * The hash table will be in TopMemoryContext regardless of caller's context.
- */
-static void
-cfunc_hashtable_init(void)
-{
-	/* don't allow double-initialization */
-	Assert(cfunc_hashtable == NULL);
-
-	cfunc_hashtable = hash_make(CachedFunctionHashEntry, key,
-								"Cached function hash", FUNCS_PER_USER,
-								.hash = cfunc_hash,
-								.match = cfunc_match,
-								.mcxt = TopMemoryContext);
-}
-
-/*
  * cfunc_hash: hash function for cfunc hash table
  *
  * We need special hash and match functions to deal with the optional
@@ -166,7 +148,11 @@ cfunc_hashtable_insert(CachedFunction *function,
 	bool		found;
 
 	if (cfunc_hashtable == NULL)
-		cfunc_hashtable_init();
+		cfunc_hashtable = hash_make(CachedFunctionHashEntry, key,
+									"Cached function hash", FUNCS_PER_USER,
+									.hash = cfunc_hash,
+									.match = cfunc_match,
+									.mcxt = TopMemoryContext);
 
 	hentry = (CachedFunctionHashEntry *) hash_search(cfunc_hashtable,
 													 func_key,
