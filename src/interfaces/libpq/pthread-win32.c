46dca7b190f7c7631099dd851d5024685b06a958
Jelte Fennema-Nio <postgres@jeltef.nl>
2025-12-13 17:09:33 +0100

    Add more pthread compatibility wrappers for windows

--- src/interfaces/libpq/pthread-win32.c
+++ src/interfaces/libpq/pthread-win32.c
@@ -12,9 +12,11 @@
 
 #include "postgres_fe.h"
 
+#include <process.h>
+
 #include "pthread-win32.h"
 
-DWORD
+pthread_t
 pthread_self(void)
 {
 	return GetCurrentThreadId();
@@ -63,4 +65,97 @@ pthread_mutex_unlock(pthread_mutex_t *mp)
 		return EINVAL;
 	LeaveCriticalSection(&mp->csection);
 	return 0;
+}
+
+/*
+ * Structure to pass arguments from pthread_create to the thread wrapper.
+ */
+typedef struct
+{
+	void	   *(*start_routine) (void *);
+	void	   *arg;
+} pthread_thread_args;
+
+/*
+ * Thread wrapper function that calls the actual start routine.
+ * Uses __stdcall calling convention required by _beginthreadex.
+ */
+static unsigned __stdcall
+pthread_thread_wrapper(void *argp)
+{
+	pthread_thread_args *args = (pthread_thread_args *) argp;
+	void	   *(*start_routine) (void *) = args->start_routine;
+	void	   *arg = args->arg;
+
+	free(args);
+
+	/* Call the actual thread function; ignore the return value */
+	(void) start_routine(arg);
+
+	return 0;
+}
+
+int
+pthread_create(pthread_t *thread, pthread_attr_t *attr,
+			   void *(*start_routine) (void *), void *arg)
+{
+	pthread_thread_args *args;
+	uintptr_t	handle;
+	unsigned	thread_id;
+
+	args = (pthread_thread_args *) malloc(sizeof(pthread_thread_args));
+	if (args == NULL)
+		return ENOMEM;
+
+	args->start_routine = start_routine;
+	args->arg = arg;
+
+	handle = _beginthreadex(NULL, 0, pthread_thread_wrapper, args, 0, &thread_id);
+	if (handle == 0)
+	{
+		free(args);
+		return errno;
+	}
+
+	/*
+	 * Store the thread ID, not the handle.  We'll need to find the handle
+	 * again in pthread_join using OpenThread.
+	 */
+	*thread = thread_id;
+
+	/* We don't need the handle now; close it to avoid leaks */
+	CloseHandle((HANDLE) handle);
+
+	return 0;
+}
+
+int
+pthread_join(pthread_t thread, void **retval)
+{
+	HANDLE		handle;
+	DWORD		result;
+
+	/* Get a handle for the thread */
+	handle = OpenThread(SYNCHRONIZE, FALSE, thread);
+	if (handle == NULL)
+		return ESRCH;
+
+	/* Wait for the thread to terminate */
+	result = WaitForSingleObject(handle, INFINITE);
+	CloseHandle(handle);
+
+	if (result != WAIT_OBJECT_0)
+		return EINVAL;
+
+	/* We don't support getting the return value */
+	if (retval != NULL)
+		*retval = NULL;
+
+	return 0;
+}
+
+void
+pthread_exit(void *retval)
+{
+	_endthreadex(0);
 }
--- src/port/pthread-win32.h
+++ src/port/pthread-win32.h
@@ -5,6 +5,8 @@
 #define __PTHREAD_H
 
 typedef ULONG pthread_key_t;
+typedef DWORD pthread_t;
+typedef void *pthread_attr_t;
 
 typedef struct pthread_mutex_t
 {
@@ -17,7 +19,7 @@ typedef struct pthread_mutex_t
 
 typedef int pthread_once_t;
 
-DWORD		pthread_self(void);
+pthread_t	pthread_self(void);
 
 void		pthread_setspecific(pthread_key_t, void *);
 void	   *pthread_getspecific(pthread_key_t);
@@ -27,5 +29,14 @@ int			pthread_mutex_lock(pthread_mutex_t *);
 
 /* blocking */
 int			pthread_mutex_unlock(pthread_mutex_t *);
+
+/* pthread_equal - compare thread IDs */
+#define pthread_equal(t1, t2)	((t1) == (t2))
+
+/* Thread creation/management - implemented in pthread-win32.c */
+int			pthread_create(pthread_t *thread, pthread_attr_t *attr,
+						   void *(*start_routine) (void *), void *arg);
+int			pthread_join(pthread_t thread, void **retval);
+void		pthread_exit(void *retval);
 
 #endif
