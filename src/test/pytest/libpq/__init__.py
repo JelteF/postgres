10fe9cac079aac7d40d070315f36595629d7bc0a
Jelte Fennema-Nio <postgres@jeltef.nl>
2025-12-16 09:25:48 +0100

    Add pytest infrastructure to interact with PostgreSQL servers
    This adds functionality to the pytest infrastructure that allows tests
    to do common things with PostgreSQL servers like:
    - creating
    - starting
    - stopping
    - connecting
    - running queries
    - handling errors
    
    The goal of this infrastructure is to be so easy to use that the actual
    tests really only contain the logic to test the behaviour that the tests
    are testing, as opposed to a bunch of boilerplate. Examples of this are:
    
    Types get converted to their Python counter parts automatically. Errors
    become actual Python exceptions. Results of queries that only return a
    single row or cell are unpacked automatically, so you don't have to do
    rows[0][0] if the query only returns a single cell.
    
    The only new tests that are part of this commit are tests that cover
    this testing infrastructure itself. It's debatable whether such tests
    are useful long term, because any infrastructure that's unused by actual
    tests should probably not exist. For now it seems good to test this
    basic functionality though, both to make sure we don't break it before
    committing actual tests that use it, and also as an example for people
    writing new tests.

---
+++ src/test/pytest/libpq/__init__.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2025, PostgreSQL Global Development Group
+
+"""
+libpq testing utilities - ctypes bindings and helpers for PostgreSQL's libpq library.
+
+This module provides Python wrappers around libpq for use in pytest tests.
+"""
+
+from . import errors
+from .errors import LibpqError
+from ._core import (
+    ConnectionStatus,
+    DiagField,
+    ExecStatus,
+    Notify,
+    PGconn,
+    PGresult,
+    PostgresMessage,
+    PostgresNotice,
+    PostgresWarning,
+    connect,
+    connstr,
+    load_libpq_handle,
+    register_type_info,
+)
+
+__all__ = [
+    "errors",
+    "LibpqError",
+    "ConnectionStatus",
+    "DiagField",
+    "ExecStatus",
+    "Notify",
+    "PGconn",
+    "PostgresMessage",
+    "PostgresNotice",
+    "PostgresWarning",
+    "PGresult",
+    "connect",
+    "connstr",
+    "load_libpq_handle",
+    "register_type_info",
+]
